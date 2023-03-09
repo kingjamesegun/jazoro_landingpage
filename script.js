97777c88288725770e5c1dc33e8e619970d20826
kingjamesegunn <james@steer.global>
2023-03-09 07:23:29 +0100

    Added JS

--- script.js
+++
@@ -1,8 +0,0 @@
-let amountInput = 0;
-amountInput = document
-	.getElementById("amount")
-	.addEventListener("change", amountInput);
-let token = (document.getElementById("token").innerHTML = amountInput * 24);
-console.log({ token });
-
-console.log({ amountInput });
